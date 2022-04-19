1e208dca573712165fc097579ae5bbfcb004556c
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-04-19 19:59:12 +0700

    setup bottom nav

--- app/src/main/java/io/github/pengdst/fintech/responsi1/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/fintech/responsi1/ui/main/MainActivity.kt
@@ -2,11 +2,22 @@ package io.github.pengdst.fintech.responsi1.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.github.pengdst.fintech.responsi1.R
+import io.github.pengdst.fintech.responsi1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
