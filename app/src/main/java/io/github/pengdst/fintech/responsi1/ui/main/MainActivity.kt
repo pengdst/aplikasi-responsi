f014fdbdc1a670b8cb051695e9eee0478988c0ef
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2022-04-19 11:42:46 +0700

    move MainActivity.kt to package ui.main

--- app/src/main/java/io/github/pengdst/fintech/responsi1/ui/main/MainActivity.kt
+++ app/src/main/java/io/github/pengdst/fintech/responsi1/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.github.pengdst.fintech.responsi1
+package io.github.pengdst.fintech.responsi1.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.pengdst.fintech.responsi1.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
